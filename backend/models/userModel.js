e555c59b0827a40132390a423f0e0b9500893d5c
unknown <nihel4688@gmail.com>
2026-07-20 20:28:24 +0100

    Set up backend and connect PostgreSQL

---
+++ backend/models/userModel.js
@@ -0,0 +1,15 @@
+const pool = require("../config/db");
+
+
+async function findUserByEmail(email) {
+    const result = await pool.query(
+        "SELECT * FROM users WHERE email = $1",
+        [email]
+    );
+
+    return result.rows[0];
+}
+
+module.exports = {
+    findUserByEmail,
+};
---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const { findUserByEmail } = require("./models/userModel");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const pool = require("./config/db");
+
+pool.query("SELECT NOW()", (err, result) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Database connected!");
+        console.log(result.rows);
+    }
+});
+async function testDatabase() {
+    const user = await findUserByEmail("admin@djezzy.dz");
+
+    console.log(user);
+}
+
+testDatabase();
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+});
