f1c408caed7fabafa33d72e1491c20c42769d218
unknown <sosarahhhh@gmail.com>
2026-07-21 22:43:43 +0100

    Merge remote changes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { register, login } = require("../controllers/authController");
+const {
+    register,
+    login,
+    requestPasswordReset
+} = require("../controllers/authController");
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/forgot-password", requestPasswordReset);
 
 module.exports = router;
